4b77268e21fbdac1ca58f84dc88e64898c91ceee
darkokos <darkosvilar02@gmail.com>
2026-03-09 00:38:22 +0100

    [add] ports of robots.txt and postcss.config.js

---
+++ postcss.config.js
@@ -0,0 +1,6 @@
+import autoprefixer from "autoprefixer";
+import postcssCustomProperties from "postcss-custom-properties";
+
+export default {
+  plugins: [autoprefixer, postcssCustomProperties],
+};
