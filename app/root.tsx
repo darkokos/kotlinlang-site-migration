75e70ca0bfcf46289edff7826241c52030ff3a16
darkokos <darkosvilar02@gmail.com>
2026-03-10 12:52:15 +0100

    [fix] scroll restoration behaviour

--- app/root.tsx
+++ app/root.tsx
@@ -130,7 +130,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           </ThemeProvider>
         </div>
 
-        <ScrollRestoration />
         <Scripts />
       </body>
     </html>
